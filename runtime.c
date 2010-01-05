51ab4a935a017294fc79a93a2cea38379bf44df4
theraven <theraven@72102866-910b-0410-8b05-ffd578937521>
2010-01-05 16:47:28 +0000

    Fixed class_getSuperclass() implementation.

--- runtime.c
+++ runtime.c
@@ -394,8 +394,13 @@ const char * class_getName(Class cls)
 	return class_get_class_name(cls);
 }
 
+void __objc_resolve_class_links(void);
 Class class_getSuperclass(Class cls)
 {
+	if (!CLS_ISRESOLV(cls))
+	{
+		__objc_resolve_class_links();
+	}
 	return class_get_super_class(cls);
 }
 
