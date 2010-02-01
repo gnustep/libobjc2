ffc041fc82524561a0fb01b78ccc81102b7b5bc2
theraven <theraven@72102866-910b-0410-8b05-ffd578937521>
2010-02-01 14:48:40 +0000

    Added missing file.

---
+++ hash_table.c
@@ -0,0 +1,14 @@
+#include "toydispatch/toydispatch.h"
+
+
+static dispatch_queue_t garbage_queue;
+
+__attribute__((constructor)) void static create_queue(void)
+{
+	garbage_queue = dispatch_queue_create("ObjC deferred free queue", 0);
+}
+
+void objc_collect_garbage_data(void(*cleanup)(void*), void *garbage)
+{
+	dispatch_async_f(garbage_queue, garbage, cleanup);
+}
