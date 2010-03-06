71be4a20f306f98a592d47fbfb17c55ee2a0ffc7
theraven <theraven@72102866-910b-0410-8b05-ffd578937521>
2010-03-06 13:46:48 +0000

    Made magic object header use #define instead of const (gets rid of unused variable warnings).

--- magic_objects.h
+++ magic_objects.h
@@ -1,7 +1,7 @@
 #ifdef GNUSTEP
-static const char *ROOT_OBJECT_CLASS_NAME = "NSObject";
-static const char *CONSTANT_STRING_CLASS = "NSConstantString";
+#define ROOT_OBJECT_CLASS_NAME "NSObject"
+#define CONSTANT_STRING_CLASS "NSConstantString"
 #else
-static const char *ROOT_OBJECT_CLASS_NAME = "Object";
-static const char *CONSTANT_STRING_CLASS = "NXConstantString";
+#define ROOT_OBJECT_CLASS_NAME "Object"
+#define CONSTANT_STRING_CLASS "NXConstantString"
 #endif
