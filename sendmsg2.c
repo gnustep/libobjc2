fb26e9a9efd1059f1b2e16aa988bedbdbaeebf83
theraven <theraven@72102866-910b-0410-8b05-ffd578937521>
2010-03-13 18:42:02 +0000

    Disable sender-aware dispatch - it breaks some code.

--- sendmsg2.c
+++ sendmsg2.c
@@ -88,9 +88,13 @@ Slot_t objc_msg_lookup_sender(id *receiver, SEL selector, id sender)
 		return &nil_slot;
 	}
 
+	/*
+	 * The self pointer is invalid in some code.  This test is disabled until
+	 * we can guarantee that it is not (e.g. with GCKit)
 	if (__builtin_expect(sender == nil
 		||
 		(sender->class_pointer->info & (*receiver)->class_pointer->info & _CLS_PLANE_AWARE),1))
+	*/
 	{
 		return objc_msg_lookup_internal(receiver, selector, sender);
 	}
