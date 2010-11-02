14ccd9b0d31da896a0041d617f7837cc7ddbbf00
theraven <theraven@72102866-910b-0410-8b05-ffd578937521>
2010-11-02 16:27:00 +0000

    Don't rely on constructor attribute - depending on undefined orderings is bad.

--- hash_table.c
+++ hash_table.c
@@ -1,14 +1,19 @@
 #include "objc/toydispatch.h"
+#include "lock.h"
 
 
 static dispatch_queue_t garbage_queue;
 
-__attribute__((constructor)) void static create_queue(void)
-{
-	garbage_queue = dispatch_queue_create("ObjC deferred free queue", 0);
-}
-
 void objc_collect_garbage_data(void(*cleanup)(void*), void *garbage)
 {
+	if (0 == garbage_queue)
+	{
+		LOCK(__objc_runtime_mutex);
+		if (0 == garbage_queue)
+		{
+			garbage_queue = dispatch_queue_create("ObjC deferred free queue", 0);
+		}
+		UNLOCK(__objc_runtime_mutex);
+	}
 	dispatch_async_f(garbage_queue, garbage, cleanup);
 }
