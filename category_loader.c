c6556fd23062e42e179edc2063285e08d38e4225
theraven <theraven@72102866-910b-0410-8b05-ffd578937521>
2011-04-19 09:57:52 +0000

    Remove FIXME that was fixed in last commit.

--- category_loader.c
+++ category_loader.c
@@ -23,8 +23,6 @@ static void register_methods(struct objc_class *cls, struct objc_method_list *l)
 	// loaded if the class hasn't received any messages yet.
 	if (classHasDtable(cls))
 	{
-		// FIXME: We can make this more efficient by simply passing the new method
-		// list to the dtable and telling it only to update those methods.
 		add_method_list_to_class(cls, l);
 	}
 }
