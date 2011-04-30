be68d812866b504ed6ac47fcb8db76db23e6ef03
theraven <theraven@72102866-910b-0410-8b05-ffd578937521>
2011-04-30 12:38:34 +0000

    Check that the LLVM source directory exists before changing to it.

--- build_opts.sh
+++ build_opts.sh
@@ -2,17 +2,19 @@
 LLVM_PATH=`llvm-config --src-root`
 LIBOBJC_PATH=`pwd`
 if [ x$LLVM_PATH != x ] ; then
-	cd $LLVM_PATH
-	cd lib/Transforms
-	if [ ! -d GNURuntime ] ; then
-		mkdir GNURuntime
-	fi
-	cd GNURuntime
-	for I in `ls $LIBOBJC_PATH/opts/` ; do
-		if [ ! $I -nt $LIBOBJC_PATH/opts/$I ] ; then
-			cp $LIBOBJC_PATH/opts/$I .
+	if [ -d $LLVM_PATH ] ; then
+		cd $LLVM_PATH
+		cd lib/Transforms
+		if [ ! -d GNURuntime ] ; then
+			mkdir GNURuntime
 		fi
-	done
-	$1 $2
-	cd ..
+		cd GNURuntime
+		for I in `ls $LIBOBJC_PATH/opts/` ; do
+			if [ ! $I -nt $LIBOBJC_PATH/opts/$I ] ; then
+				cp $LIBOBJC_PATH/opts/$I .
+			fi
+		done
+		$1 $2
+		cd ..
+	fi
 fi
