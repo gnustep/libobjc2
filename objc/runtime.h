8e34dccda973d160e583469e3ef289f4b0bf709d
theraven <theraven@72102866-910b-0410-8b05-ffd578937521>
2011-07-27 10:39:48 +0000

    Deprecate access to the isa pointer.  This will become a hard error in future.

--- objc/runtime.h
+++ objc/runtime.h
@@ -76,6 +76,9 @@ typedef struct objc_object
 	 * Pointer to this object's class.  Accessing this directly is STRONGLY
 	 * discouraged.  You are recommended to use object_getClass() instead.
 	 */
+#ifndef __OBJC_RUNTIME_INTERNAL__
+	__attribute__((deprecated))
+#endif
 	Class isa;
 } *id;
 
