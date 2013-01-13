63bdbc94409e9a01a111f00624dc4d1c5981a939
theraven <theraven@72102866-910b-0410-8b05-ffd578937521>
2013-01-13 17:21:16 +0000

    Add another missing file...

---
+++ Test/Test.h
@@ -0,0 +1,23 @@
+#import "objc/runtime.h"
+#include <assert.h>
+
+#ifndef __has_attribute
+#define __has_attribute(x) 0
+#endif
+
+#if __has_attribute(objc_root_class)
+__attribute__((objc_root_class))
+#endif
+@interface Test { id isa; }
+@end
+@implementation Test
++ (id)class { return self; }
++ (id)new
+{
+	return class_createInstance(self, 0);
+}
+- (void)dealloc
+{
+	object_dispose(self);
+}
+@end
