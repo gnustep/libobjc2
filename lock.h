0ed2182b8322a565494701854377f54106dbeb55
theraven <theraven@72102866-910b-0410-8b05-ffd578937521>
2013-05-28 10:00:11 +0000

    Add explicit cast for pthread lock initialiser.

--- lock.h
+++ lock.h
@@ -23,7 +23,7 @@ typedef pthread_mutex_t mutex_t;
 // If this pthread implementation has a static initializer for recursive
 // mutexes, use that, otherwise fall back to the portable version
 #	ifdef PTHREAD_RECURSIVE_MUTEX_INITIALIZER_NP
-#		define INIT_LOCK(x) x = PTHREAD_RECURSIVE_MUTEX_INITIALIZER_NP
+#		define INIT_LOCK(x) x = (pthread_mutex_t)PTHREAD_RECURSIVE_MUTEX_INITIALIZER_NP
 #	elif defined(PTHREAD_RECURSIVE_MUTEX_INITIALIZER)
 #		define INIT_LOCK(x) x = (pthread_mutex_t)PTHREAD_RECURSIVE_MUTEX_INITIALIZER
 #	else
