430ab750144256c2b9d4caef991e981102610402
Dustin L. Howett <duhowett@microsoft.com>
2016-01-04 10:20:50 -0800

    win32: use native threading APIs instead of pthreads
    * use fiber local storage if NO_PTHREADS is defined
    * use critical sections instead of mutexes
    
    Contributing-author: Ben Viglietta <benvi@microsoft.com>

--- lock.h
+++ lock.h
@@ -6,15 +6,13 @@
 
 #ifndef __LIBOBJC_LOCK_H_INCLUDED__
 #define __LIBOBJC_LOCK_H_INCLUDED__
-#ifdef WIN32
-#define BOOL _WINBOOL
-#	include <windows.h>
-#undef BOOL
-typedef HANDLE mutex_t;
-#	define INIT_LOCK(x) x = CreateMutex(NULL, FALSE, NULL)
-#	define LOCK(x) WaitForSingleObject(*x, INFINITE)
-#	define UNLOCK(x) ReleaseMutex(*x)
-#	define DESTROY_LOCK(x) CloseHandle(*x)
+#ifdef _WIN32
+#	include "safewindows.h"
+typedef CRITICAL_SECTION mutex_t;
+#	define INIT_LOCK(x) InitializeCriticalSection(&(x))
+#	define LOCK(x) EnterCriticalSection(x)
+#	define UNLOCK(x) LeaveCriticalSection(x)
+#	define DESTROY_LOCK(x) DeleteCriticalSection(x)
 #else
 
 #	include <pthread.h>
