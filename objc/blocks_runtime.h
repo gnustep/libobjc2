34cd09a538572276eafe2e632b7be4c23c27d476
Daniel Ferreira <bnmvco@gmail.com>
2017-06-01 21:58:30 -0300

    blocks_runtime: create aliases for type encoding getters
    Add _Block_has_signature() and _Block_signature() alises for calls to
    block_getType_np() for increased compatibility with other
    implementations of libobjc2.

--- objc/blocks_runtime.h
+++ objc/blocks_runtime.h
@@ -2,6 +2,10 @@
 #pragma clang system_header
 #endif
 
+#ifndef __has_feature
+#	define __has_feature(x) 0
+#endif
+
 /*
  * Blocks Runtime
  */
@@ -19,3 +23,14 @@ BLOCKS_EXPORT const char *block_getType_np(void *b) OBJC_NONPORTABLE;
 
 #define Block_copy(x) ((__typeof(x))_Block_copy((void *)(x)))
 #define Block_release(x) _Block_release((void *)(x))
+
+/*
+ * Aliases for block signature macros
+ */
+#if __has_feature(objc_arc)
+#define _Block_signature(x) block_getType_np((__bridge void *)(x))
+#define _Block_has_signature(x) (block_getType_np((__bridge void *)(x)) != NULL)
+#else
+#define _Block_signature(x) block_getType_np((void *)(x))
+#define _Block_has_signature(x) (block_getType_np((void *)(x)) != NULL)
+#endif
