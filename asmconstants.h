f597bfebeedc1fda82a71a87479e4756a32c3914
Sabine-Schnabeltier <34526618+Sabine-Schnabeltier@users.noreply.github.com>
2017-12-14 17:59:52 -0600

    add offsets for LLP64

--- asmconstants.h
+++ asmconstants.h
@@ -4,6 +4,12 @@
 #define SHIFT_OFFSET   0
 #define DATA_OFFSET    8
 #define SLOT_OFFSET    32
+#elif (__SIZEOF_LONG__ == 4) && (__SIZEOF_POINTER__ == 8)  /* LLP64 */
+#define DTABLE_OFFSET  52   /* 5 pointers + 3 longs */
+#define SMALLOBJ_BITS  3
+#define SHIFT_OFFSET   0
+#define DATA_OFFSET    8
+#define SLOT_OFFSET    28   /* 3 pointers + 1 long */
 #else
 #define DTABLE_OFFSET  32
 #define SMALLOBJ_BITS  1
