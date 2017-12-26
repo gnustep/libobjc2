7832765112af58f5777c22c9b44832a9df44d0e3
David Chisnall <David.Chisnall@cl.cam.ac.uk>
2017-12-26 17:32:50 +0000

    Improve detection of libsupc++

--- CMake/typeinfo_test.cc
+++ CMake/typeinfo_test.cc
@@ -21,7 +21,6 @@ namespace std
 				bool operator==(const type_info &) const;
 				bool operator!=(const type_info &) const;
 				bool before(const type_info &) const;
-				type_info();
 				private:
 				type_info(const type_info& rhs);
 				type_info& operator= (const type_info& rhs);
@@ -44,14 +43,12 @@ namespace std
 class type_info2 : public std::type_info
 {
 	public:
+	type_info2() : type_info("foo") {}
 	virtual bool __is_pointer_p() const; 
 	virtual bool __is_function_p() const { return true; }
 	virtual bool __do_catch(const type_info *thrown_type,
 	                        void **thrown_object,
 	                        unsigned outer) const { return true; }
-	virtual bool __do_upcast(
-	                const __class_type_info *target,
-	                void **thrown_object) const { return true; }
 };
 bool type_info2::__is_pointer_p() const { return true; }
 
