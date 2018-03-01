81f7b16af5f9e84d22d85e7a947a40594fd2c785
Dustin Howett <duhowett@microsoft.com>
2018-02-28 17:14:19 -0800

    Add a test for e3b069c (association policy)

--- Test/AssociatedObject.m
+++ Test/AssociatedObject.m
@@ -19,8 +19,21 @@ int main(void)
 	@autoreleasepool {
 		Associated *object = [Associated new];
 		Test *holder = [[Test new] autorelease];
-		objc_setAssociatedObject(object, &objc_setAssociatedObjectKey, holder, OBJC_ASSOCIATION_RETAIN);
+		objc_setAssociatedObject(holder, &objc_setAssociatedObjectKey, object, OBJC_ASSOCIATION_RETAIN);
 		[object release];
-    }
+		assert(!deallocCalled);
+	}
+	// dealloc should be called when holder is released during pool drain
+	assert(deallocCalled);
+
+	deallocCalled = NO;
+
+	Associated *object = [Associated new];
+	Test *holder = [Test new];
+	objc_setAssociatedObject(holder, &objc_setAssociatedObjectKey, object, OBJC_ASSOCIATION_RETAIN);
+	[object release]; // commuted into associated object storage
+	objc_setAssociatedObject(holder, &objc_setAssociatedObjectKey, nil, OBJC_ASSOCIATION_ASSIGN);
+	[holder release];
+
 	assert(deallocCalled);
 }
