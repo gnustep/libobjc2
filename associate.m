828e6ebf70f85d098da0c627c6fe097d76ae77af
Dustin Howett <duhowett@microsoft.com>
2018-03-02 18:52:57 -0800

    always look beyond the first page of associated objects
    Without this fix, we would lose associated objects silently after adding
    the 11th. We would also allocate full pages for each object after the
    11th because we couldn't find empty slots.

--- associate.m
+++ associate.m
@@ -75,14 +75,16 @@ static BOOL isAtomic(uintptr_t policy)
 
 static struct reference* findReference(struct reference_list *list, void *key)
 {
-	if (NULL == list) { return NULL; }
-
-	for (int i=0 ; i<REFERENCE_LIST_SIZE ; i++)
+	while (list)
 	{
-		if (list->list[i].key == key)
+		for (int i=0 ; i<REFERENCE_LIST_SIZE ; i++)
 		{
-			return &list->list[i];
+			if (list->list[i].key == key)
+			{
+				return &list->list[i];
+			}
 		}
+		list = list->next;
 	}
 	return NULL;
 }
