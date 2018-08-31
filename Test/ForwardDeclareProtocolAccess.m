262cea96f5b9ca582aeb934afa0dbf8560b60529
David Chisnall <github@theravensnest.org>
2018-08-31 12:31:10 +0100

    Fix test for clang 8.
    Clang 8 now rejects the thing that this is testing (which is good,
    because it's a terrible idea).

--- Test/ForwardDeclareProtocolAccess.m
+++ Test/ForwardDeclareProtocolAccess.m
@@ -11,6 +11,10 @@ int main(void)
 {
 	Protocol *p1 = @protocol(P);
 	Protocol *p2 = getProtocol();
+	if (p2 == NULL)
+	{
+		return 0;
+	}
 	assert(protocol_isEqual(p1, p2));
 #ifdef GS_RUNTIME_V2
 	// With the new ABI, these should be precisely the same object.
