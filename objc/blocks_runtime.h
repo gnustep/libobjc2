6e3c618b8257ff56b1a18fd47acaedcd84d123d8
Jordan Schidlowsky <jordan@noodlecake.com>
2019-07-10 11:43:56 -0600

    cleanup in blocks runtime for closer consistency with apple

--- objc/blocks_runtime.h
+++ objc/blocks_runtime.h
@@ -7,22 +7,19 @@
  * Blocks Runtime
  */
 #include "Availability.h"
+#include <stdbool.h>
 #ifdef __cplusplus
-#define OBJC_BOOL_TYPE bool
-#define OBJC_BLOCK_PTR_TYPE void*
 #define BLOCKS_EXPORT extern "C"
 #else
-#define OBJC_BOOL_TYPE _Bool
-#define OBJC_BLOCK_PTR_TYPE id
-#define BLOCKS_EXPORT extern 
+#define BLOCKS_EXPORT extern
 #endif
 
 OBJC_PUBLIC BLOCKS_EXPORT void *_Block_copy(const void *);
 OBJC_PUBLIC BLOCKS_EXPORT void _Block_release(const void *);
 OBJC_PUBLIC BLOCKS_EXPORT const char *block_getType_np(const void *b) OBJC_NONPORTABLE;
 
-OBJC_PUBLIC BLOCKS_EXPORT OBJC_BOOL_TYPE _Block_has_signature(OBJC_BLOCK_PTR_TYPE);
-OBJC_PUBLIC BLOCKS_EXPORT const char * _Block_signature(OBJC_BLOCK_PTR_TYPE);
+OBJC_PUBLIC BLOCKS_EXPORT bool _Block_has_signature(void *);
+OBJC_PUBLIC BLOCKS_EXPORT const char * _Block_signature(void *);
 
 
 #define Block_copy(x) ((__typeof(x))_Block_copy((const void *)(x)))
