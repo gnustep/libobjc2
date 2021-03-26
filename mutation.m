2d976b61e2bd491cfc8c506b96589f56e7e4188a
Frederik Seiffert <frederik@algoriddim.com>
2021-03-26 18:06:36 +0100

    Export objc_enumerationMutation() function.

--- mutation.m
+++ mutation.m
@@ -4,6 +4,7 @@
 
 // This function is exported as a weak symbol to enable GNUstep or some other
 // framework to replace it trivially
+OBJC_PUBLIC
 void __attribute__((weak)) objc_enumerationMutation(id obj)
 {
 	fprintf(stderr, "Mutation occured during enumeration.");
