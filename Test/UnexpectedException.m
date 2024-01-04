f631f0c840dfe8648c0943e36881c4006ff4419f
Frederik Carlier <frederik.carlier@keysight.com>
2024-01-04 07:56:53 -0800

    Use C++ exceptions unconditionally for Objective-C[++] on MinGW

--- Test/UnexpectedException.m
+++ Test/UnexpectedException.m
@@ -8,19 +8,20 @@
 #include <Windows.h>
 #endif
 
-id exceptionObj = @"Exception";
+id expectedExceptionObj = @"ExpectedException";
+id unexpectedExceptionObj = @"UnexpectedException";
 
 void _UncaughtExceptionHandler(id exception)
 {
-	assert(exception == exceptionObj);
-#ifdef _WIN32
+	assert(exception == unexpectedExceptionObj);
+#if defined(_WIN32) && !defined(__MINGW32__)
 	// on Windows we will exit in _UnhandledExceptionFilter() below
 #else
 	exit(0);
 #endif
 }
 
-#ifdef _WIN32
+#if defined(_WIN32) && !defined(__MINGW32__)
 LONG WINAPI _UnhandledExceptionFilter(struct _EXCEPTION_POINTERS* exceptionInfo)
 {
 	assert(exceptionInfo != NULL);
@@ -30,18 +31,26 @@ LONG WINAPI _UnhandledExceptionFilter(struct _EXCEPTION_POINTERS* exceptionInfo)
 
 int main(void)
 {
-	#if !(defined(__arm__) || defined(__ARM_ARCH_ISA_A64))
-#ifdef _WIN32
+#if !(defined(__arm__) || defined(__ARM_ARCH_ISA_A64))
+#if defined(_WIN32) && !defined(__MINGW32__)
 	// also verify that an existing handler still gets called after we set ours
 	SetUnhandledExceptionFilter(&_UnhandledExceptionFilter);
 #endif
+	@try
+	{
+		@throw expectedExceptionObj;
+	}
+	@catch(id exception)
+	{
+		assert(exception == expectedExceptionObj);
+	}
 
 	objc_setUncaughtExceptionHandler(_UncaughtExceptionHandler);
-	@throw exceptionObj;
+	@throw unexpectedExceptionObj;
 	assert(0 && "should not be reached!");
 
 	return -1;
-	#endif
+#endif
 	// FIXME: Test currently fails on ARM and AArch64
 	return 77; // Skip test
 }
